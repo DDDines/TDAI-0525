87690ce58051acfa41cf4453477e9c34c206814d
Julio Barizon <83961298+DDDines@users.noreply.github.com>
2025-06-11 12:17:43 +0200

    Add pytest and asyncio plugin

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 pip install -r requirements-backend.txt
+pip install pytest pytest-asyncio
 export FIRST_SUPERUSER_EMAIL="admin@example.com"
 export FIRST_SUPERUSER_PASSWORD="adminpass"
 export ADMIN_EMAIL="admin@example.com"
